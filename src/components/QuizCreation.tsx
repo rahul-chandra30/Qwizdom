44826538044b281a0bac9fe0e5914d440309db81
Rahul Chandra <rahulninza@gmail.com>
2024-09-18 07:05:32 +0530

    Remove .env from the repository -2

--- src/components/QuizCreation.tsx
+++ src/components/QuizCreation.tsx
@@ -1,17 +1,33 @@
 import React from 'react'
-import { Card, CardDescription, CardHeader, CardTitle } from './ui/card'
-
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './ui/card'
+import { useForm } from 'react-hook-form'
+import { quizCreationSchema } from '@/schemas/form/quiz'
+import { z } from 'zod'
 type Props = {}
 
+type input = z.infer<typeof quizCreationSchema>
+
 const QuizCreation = (props: Props) => {
+    const form = useForm<Input>({
+        resolver: zodResolver(quizCreationSchema),
+        defaultValues: {
+            topic: '',
+            type: 'mcq',
+            amount: 1
+        }
+    })
+
   return (
       <div className="absolute -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2 ">
           <Card>
               <CardHeader>
                   <CardTitle className="text-2x font-bold">Quiz Creation</CardTitle>
                   <CardDescription>
-                       Choose a topic.
+                       Choose a topic. 
                   </CardDescription>
+                  <CardContent>
+                      
+                  </CardContent>
               </CardHeader>
           </Card>
       </div>
--- src/schemas/form/quiz.ts
+++ src/schemas/form/quiz.ts
@@ -3,5 +3,5 @@ import { z } from 'zod'
 export const quizCreationSchema = z.object({
   topic: z.string().min(4, {message: 'Topic must be 4 characters long'}).max(50),
     type: z.enum(['mcq', 'open_ended']),
-  amount: z.number().min(1).max(11),
+  amount: z.number().min(1).max(10),
 });
